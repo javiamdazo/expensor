1f45c5febff6530ac2f035a98dea7e5aae806aac
JavierAndres <jandres@visualnacert.com>
2024-09-20 13:29:15 +0200

    add new dashboard

--- lib/pages/dashboard/app_bar_widget.dart
+++ lib/pages/dashboard/app_bar_widget.dart
@@ -1,53 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-CupertinoNavigationBar buildCustomCupertinoNavigationBar(BuildContext context) {
+CupertinoNavigationBar buildCustomCupertinoNavigationBar(BuildContext context, String title) {
   final brightness = CupertinoTheme.of(context).brightness;
 
   return CupertinoNavigationBar(
-      backgroundColor: brightness == Brightness.dark
-                      ? CupertinoColors.black
-                      : Colors.white,
-      leading: GestureDetector(
-        onTap: () {
-          // Show the drawer or perform your desired action
-          // Here you might implement a modal or another way to show navigation options
-        },
-        child: Icon(CupertinoIcons.bars, color: brightness == Brightness.dark
-                      ? Colors.white
-                      : Colors.black,),
-      ),
-      middle: Row(
-        children: [
-          CircleAvatar(
-            backgroundColor: CupertinoColors.activeBlue,
-            child: Text("JA", style: TextStyle(color: brightness == Brightness.dark
-                      ? Colors.black
-                      : Colors.white)),
-          ),
-          const SizedBox(width: 10),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Hola de nuevo',
-                style: TextStyle(fontSize: 10,color: brightness == Brightness.dark
-                      ? Colors.white
-                      : Colors.black),
-              ),
-              Text('Javier Andrés', style: TextStyle(color: brightness == Brightness.dark
-                      ? Colors.white
-                      : Colors.black),),
-            ],
-          ),
-        ],
+      backgroundColor:
+          brightness == Brightness.dark ? CupertinoColors.black : Colors.white,
+      automaticallyImplyLeading: false,
+      middle: Text(
+        title,
+        style: TextStyle(
+            fontSize: 18,
+            color: brightness == Brightness.dark ? Colors.white : Colors.black),
       ),
       trailing: GestureDetector(
         onTap: () {
           // Handle notifications tap
         },
-        child: Icon(CupertinoIcons.gear, color: brightness == Brightness.dark
-                      ? Colors.white
-                      : Colors.black,),
+        child: Icon(
+          CupertinoIcons.person,
+          color: brightness == Brightness.dark ? Colors.white : Colors.black,
+        ),
       ));
 }
--- lib/pages/dashboard/dashboard_page.dart
+++ lib/pages/dashboard/dashboard_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:expensor/pages/dashboard/app_bar_widget.dart';
-import 'package:expensor/pages/dashboard/drawer_widget.dart';
-import 'package:expensor/pages/dashboard/floating_button_widget.dart';
+import 'package:super_cupertino_navigation_bar/super_cupertino_navigation_bar.dart';
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
@@ -14,6 +12,9 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   int currentIndex = 0;
 
+  int? selectedIndexFilter;
+  List<String> filters = ['Casa', 'Coche', 'Restaurantes'].toList();
+
   void handleScreenChanged(int selectedScreen) {
     setState(() {
       currentIndex = selectedScreen;
@@ -22,16 +23,109 @@ class _DashboardPageState extends State<DashboardPage> {
 
   @override
   Widget build(BuildContext context) {
-    var screenWidth = MediaQuery.of(context).size.width;
+    var colorScheme = Theme.of(context).colorScheme;
 
-    return CupertinoPageScaffold(
-      navigationBar: buildCustomCupertinoNavigationBar(context),
-      child: const Center(
-        child: Text('Dashboard Page'),
-      ),
-      // Cupertino does not have a built-in Drawer; we'll use a modal for side navigation
-      // Note: Implement your own modal or side menu as needed
-    );
+    return Scaffold(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        body: SuperScaffold(
+          appBar: SuperAppBar(
+            title: const Text(
+              "Dashboard",
+              style: TextStyle(color: Colors.black),
+            ),
+            largeTitle: SuperLargeTitle(
+              enabled: true,
+              largeTitle: "Dashboard",
+            ),
+            actions: IconButton(
+                onPressed: () => {}, icon: const Icon(CupertinoIcons.person)),
+            searchBar: SuperSearchBar(
+              enabled: true,
+              onChanged: (query) {
+                // Search Bar Changes
+              },
+              onSubmitted: (query) {
+                // On Search Bar submitted
+              },
+              // Add other search bar properties as needed
+            ),
+            bottom: SuperAppBarBottom(
+              child: Padding(
+                  padding: const EdgeInsets.only(left: 15, right: 15),
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: filters.length,
+                    itemBuilder: (context, index) {
+                      bool isSelected = selectedIndexFilter == index;
+
+                      return Row(children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: isSelected
+                                ? colorScheme.primary
+                                : Colors
+                                    .transparent, // Fondo transparente si no está seleccionado
+                            border: Border.all(
+                              color: colorScheme
+                                  .primary, // Borde del color primario
+                              width: 2, // Grosor del borde
+                            ),
+                            borderRadius:
+                                BorderRadius.circular(20), // Borde redondeado
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10, right: 10),
+                            child: Row(children: [
+                            Icon(CupertinoIcons.home, color: isSelected
+                                      ? Colors.white
+                                      : colorScheme
+                                          .primary,),
+                            CupertinoButton(
+                              borderRadius: BorderRadius.circular(20),
+                              onPressed: () {
+                                // Al presionar el botón, actualizamos el índice seleccionado
+                                setState(() {
+                                  selectedIndexFilter = index;
+                                });
+                              },
+                              padding:
+                                  const EdgeInsets.only(left: 10, right: 10),
+                              // Cambiamos el estilo del texto según si el botón está seleccionado o no
+                              child: Text(
+                                filters[index],
+                                style: TextStyle(
+                                  color: isSelected
+                                      ? Colors.white
+                                      : colorScheme
+                                          .primary, // Texto blanco si está seleccionado, primario si no
+                                ),
+                              ),
+                            )
+                          ]),
+                        )),
+                        const SizedBox(width: 10),
+                      ]);
+                    },
+                  )),
+              enabled: true,
+            ),
+          ),
+          body: const SingleChildScrollView(
+            // You can use CustomScrollView or any Widget desired
+            child: Column(
+              children: [
+                Card(
+                    // Playground card
+                    // Add your onTap logic for Playground card
+                    ),
+                Card(
+                    // List of example cards
+                    // Add your ListView.separated logic for example cards
+                    ),
+              ],
+            ),
+          ),
+        ));
   }
 
   // Build custom floating action button as a Cupertino Button
